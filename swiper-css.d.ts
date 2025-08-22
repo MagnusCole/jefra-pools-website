5d00ca89942ae5b139f4afac1faeecf089f95e03
Ghost <muerasgary@gmail.com>
2025-08-21 23:12:07 -0500

    chore: add Swiper CSS type declarations and fix build

---
+++ swiper-css.d.ts
@@ -0,0 +1,5 @@
+// Global declarations for Swiper CSS side-effect imports (picked up via tsconfig include)
+declare module 'swiper/css';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/*';
